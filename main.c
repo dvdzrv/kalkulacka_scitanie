af678e87be29ecb4f328dc2a51cf8fcf9d33e8be
dvdzrv <david.szomolai@sostrv.sk>
2023-10-27 23:52:55 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double a,b,c;
+    printf("Zadaj prve cislo:");
+    scanf("%lf", &a);
+    printf("Zadaj druhe cislo:");
+    scanf("%lf", &b);
+    c= a+b;
+    printf("Vysledok: %f", c);
+    printf("\nStlac ENTER na ukoncenie :)\n");
+    getch();
+    return 0;
+}
